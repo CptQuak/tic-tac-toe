b0bc79cb382466cbcb8b31480f096d2a7e8f8699
Quak <kamilkulesza5@gmail.com>
2021-10-10 16:25:25 +0200

    finding winner and updating title

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = (function () {
     let board = ["", "", "", "", "", "", "", "", ""];
+    let gameState = true;
     //cache dom
     const cells = document.querySelectorAll(".card");
     cells.forEach((cell) => {
@@ -12,16 +13,18 @@ const gameBoard = (function () {
     //         cell.textContent = board[Number(cell.dataset.cell - 1)];
     //     });
     // }
-    function replace(event, currentPlayer) {
-        if (event.target.textContent == "") {
-            event.target.textContent = "X";
-            board[event.target.dataset.cell - 1] = event.target.textContent;
+    function replace(event, player) {
+        if (gameState && event.target.textContent == "") {
+            board[event.target.dataset.cell - 1] = "X";
+            event.target.textContent = board[event.target.dataset.cell - 1];
         }
         stopGame(displayController.gameState);
     }
-    function stopGame(gameState) {
-        console.log(board);
-        if (board.includes("X")) {
+    function stopGame() {
+        //need logic
+        if (board[0] == "X" && board[1] == "X" && board[2] == "X") {
+            gameState = false;
+            displayController.updateDisplay("Quak");
         }
     }
     return {
@@ -30,10 +33,13 @@ const gameBoard = (function () {
 })();
 
 const displayController = (function () {
-    let gameState = true;
     const display = document.querySelector(".display");
+    //button to reset board using board api
+    function updateDisplay(player) {
+        display.textContent = `${player} won`;
+    }
 
-    return { gameState };
+    return { updateDisplay };
 })();
 
 const playerFactory = (name, sign) => {
@@ -42,7 +48,5 @@ const playerFactory = (name, sign) => {
     return { name, sign, playerTurn, isWinner };
 };
 
-(() => {
-    const player1 = playerFactory("player1", "X");
-    const player2 = playerFactory("player2", "O");
-})();
+const player1 = playerFactory("player1", "X");
+const player2 = playerFactory("player2", "O");
