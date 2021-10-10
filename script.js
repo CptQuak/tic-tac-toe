8aa75b8a9d723c59159e216775e3087f1b99044d
Quak <kamilkulesza5@gmail.com>
2021-10-10 17:46:08 +0200

    tic-tac-toe game logic added

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const playerFactory = (name, sign) => {
 };
 
 const player1 = playerFactory("player122", "X");
+player1.playerTurn = true;
 const player2 = playerFactory("player2", "O");
 
 const gameBoard = (function () {
@@ -51,28 +52,64 @@ const gameBoard = (function () {
             board[event.target.dataset.cell - 1] = currentPlayer.sign;
             event.target.textContent = board[event.target.dataset.cell - 1];
         }
+        console.log(player1.playerTurn);
+        console.log(player2.playerTurn);
+        console.log(currentPlayer);
+
         decideWinner();
-        // nextPlayer();
+        gameState && nextPlayer();
+
         // getCurrentPlayer();
     }
     function decideWinner() {
         //need logic
-        if (board[0] == "X" && board[1] == "X" && board[2] == "X") {
+        if (
+            (board[0] == currentPlayer.sign &&
+                board[1] == currentPlayer.sign &&
+                board[2] == currentPlayer.sign) ||
+            (board[3] == currentPlayer.sign &&
+                board[4] == currentPlayer.sign &&
+                board[5] == currentPlayer.sign) ||
+            (board[6] == currentPlayer.sign &&
+                board[7] == currentPlayer.sign &&
+                board[8] == currentPlayer.sign) ||
+            (board[0] == currentPlayer.sign &&
+                board[3] == currentPlayer.sign &&
+                board[6] == currentPlayer.sign) ||
+            (board[1] == currentPlayer.sign &&
+                board[4] == currentPlayer.sign &&
+                board[7] == currentPlayer.sign) ||
+            (board[2] == currentPlayer.sign &&
+                board[5] == currentPlayer.sign &&
+                board[8] == currentPlayer.sign) ||
+            (board[0] == currentPlayer.sign &&
+                board[4] == currentPlayer.sign &&
+                board[8] == currentPlayer.sign) ||
+            (board[2] == currentPlayer.sign &&
+                board[4] == currentPlayer.sign &&
+                board[6] == currentPlayer.sign)
+        ) {
             gameState = false;
             // events.emit("displayButton");
             events.emit("winnerFound", currentPlayer);
         }
     }
 
-    function nextPlayer() {}
+    function nextPlayer() {
+        player1.playerTurn = !player1.playerTurn;
+        player2.playerTurn = !player2.playerTurn;
+        currentPlayer = player1.playerTurn ? player1 : player2;
+        events.emit("setplayer", currentPlayer);
+    }
     function getCurrentPlayer() {}
     function gameRestart() {
         // events.emit("displayButton");
         board = ["", "", "", "", "", "", "", "", ""];
         gameState = true;
         currentPlayer = player1;
+        player1.playerTurn = true;
         cells.forEach((cell, index) => (cell.textContent = `${board[index]}`));
-        // events.emit("setplayer");
+        events.emit("setplayer", currentPlayer);
     }
     return {
         board,
